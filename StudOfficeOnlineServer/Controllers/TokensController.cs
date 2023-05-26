0d33658d1cc5185dc2b7f181e7db269a81fe27af
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2023-05-26 19:36:39 +0300

    DBConext has been registered in service collection at Program.cs

---
+++ StudOfficeOnlineServer/Controllers/TokensController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudOfficeOnlineServer.Models;
+
+namespace StudOfficeOnlineServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TokensController : ControllerBase
+    {
+        private readonly DBContext _db;
+        private readonly IConfiguration _configuration;
+
+        public TokensController(DBContext db, IConfiguration configuration)
+        {
+            _db = db;
+            _configuration = configuration;
+        }
+
+
+    }
+}
