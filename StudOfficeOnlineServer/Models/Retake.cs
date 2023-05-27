69ae7e8eac791bf2ad585d69d6991d9907eb958d
kiespetch1 <kiespetch@ya.ru>
2023-05-27 08:03:27 +0300

    add retake controller

---
+++ StudOfficeOnlineServer/Models/Retake.cs
@@ -0,0 +1,15 @@
+﻿namespace StudOfficeOnlineServer.Models;
+
+public class Retake
+{
+    public int Id { get; set; }
+    public string Subject { get; set; }
+
+    public int TeacherId { get; set; }
+
+    public int GroupId { get; set; }
+
+    public DateTime DateTime { get; set; }
+
+    public string Сlassroom { get; set; }
+}
