b836043363e284d168c1c738788aa946097b242a
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2023-05-27 07:43:55 +0300

    Added cors

--- StudOfficeOnlineServer/Program.cs
+++ StudOfficeOnlineServer/Program.cs
@@ -6,12 +6,12 @@ using StudOfficeOnlineServer.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-/*
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy => policy.WithOrigins(builder.Configuration["Cors:Origin"]!).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
 });
-*/
+
 string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
 builder.Services.AddDbContext<DBContext>(options => options.UseNpgsql(connection));
 builder.Services.AddStackExchangeRedisCache(options =>
@@ -45,7 +45,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthentication();
-//app.UseMiddleware<JWTCheckMiddleware>();
+app.UseMiddleware<JWTCheckMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
